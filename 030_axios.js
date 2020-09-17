fef855cb46d39feffe8494e2723697d6ad11b54c
iahmed <ahmedisra92@gmail.com>
2020-09-17 15:17:24 +0200

    finAxios

--- 030_axios.js
+++ 030_axios.js
@@ -1,6 +1,18 @@
 var app= new Vue({
     el: '#totoro',    
     mounted(){
-        window.axios.get('https://pokeapi.co/api/v2/pokemon/1/').then(response => console.log(response.data));
+        window.axios.get('https://pokeapi.co/api/v2/pokemon/1/').then(response => {
+            console.log(response.data);
+            console.log('Le nom du Pokemon est: '+response.data.name);
+            this.statistiques = response.data.stats ;
+            console.log(this.statistiques) ;
+            this.statistiques.forEach(unElement => {
+                console.log('Le nom est : '+ unElement.stat.name) ;
+                console.log('Qui a pour valeur: '+ unElement.base_stat) ;
+
+        });
+            console.log('La stat defense vaut '+this.statistiques[2].base_stat);
+    });
+
     }
 });
