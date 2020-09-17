899d007ec3032d090f15ff8378d71cbe1e75d743
iahmed <ahmedisra92@gmail.com>
2020-09-17 14:25:18 +0200

    bonjour

--- 030_axios.js
+++ 030_axios.js
@@ -1,6 +1,6 @@
 var app= new Vue({
     el: '#totoro',    
     mounted(){
-        
+        window.axios.get('https://pokeapi.co/api/v2/pokemon/1/').then(response => console.log(response.data));
     }
 });
